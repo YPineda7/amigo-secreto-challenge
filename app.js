55f37288835d157a4361581b759198a919d05286
Yoselin <yos_hdz.pi@icloud.com>
2025-01-31 11:20:33 -0700

    funcion para optener nombre funcionando

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+//funcion para recibir el nombre funcionando 
 function agregarAmigo(nombre){
     let amigos = document.getElementById('amigo').value;
     console.log(amigos)
