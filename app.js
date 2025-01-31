9d00b3d986c98af8f5726d3e0f04c8eb9795084c
Yoselin <yos_hdz.pi@icloud.com>
2025-01-31 16:37:57 -0700

    agregar elementos li

--- app.js
+++ app.js
@@ -9,15 +9,31 @@ function agregarAmigo(){
         alert('Por favor inserte un nombre');
     }else{
         //Agregar con un push nombres al array
-      amigos.push(amigosNombre);
-      console.log(amigos);
-      limpiarCaja;
+        amigos.push(amigosNombre);
+        agregarALista();
+     
     }
+    limpiarCaja('amigo');
     return;
 }
 //funcion limpiar caja
-function limpiarCaja(){
-    document.getElementById('amigo').value = '';
+function limpiarCaja(elemento){
+    document.getElementById(elemento).value = '';
 }
 
-agregarAmigo();
+//agrega elementos en li
+function agregarALista() {
+    let lista = document.getElementById('listaAmigos');
+    //limpiamos para que no haya duplicados
+    lista.innerHTML ='';
+    //bucle para seguir preguntando nombres
+    for(let i =0 ;i < amigos.length; i++){
+    let elementoListaHTML = document.createElement('li');
+    elementoListaHTML.textContent = amigos[i];
+    lista.appendChild(elementoListaHTML);
+    }
+}
+
+
+
+
